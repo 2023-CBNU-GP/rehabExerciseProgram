de2b953000677a90529a42855555373efbc4d986
songsuyoung <tndud3999@naver.com>
2023-03-17 12:30:27 +0900

    중앙 위치선 표시

--- PoseDetector.py
+++ PoseDetector.py
@@ -1,6 +1,5 @@
 import mediapipe as mp
 import cv2
-import time
 
 class PoseDetector() :
 
--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import cv2
-import time
 import PoseDetector as pd
+import ImageOverlayer as io
 
-cap = cv2.VideoCapture("OpenCV_video1.mp4")
-pTime = 0
-detector = pd.PoseDetector()
-while True:
-    success, img = cap.read()
-    img = detector.findPose(img)
-    lmList = detector.findPosition(img)
+webcam = cv2.VideoCapture(0)
+personFrame = cv2.imread("PersonFrame.png",cv2.IMREAD_UNCHANGED)
+h,w,_=personFrame.shape
 
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
+overlay=io.ImageOverlayer()
+if not webcam.isOpened() :
+    exit()
 
-    cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
-    cv2.imshow("image", img)
-    cv2.waitKey(1)
+while webcam.isOpened():
+    lmList=[]
+    detector = pd.PoseDetector()
+    status, frame = webcam.read()
+
+    if status :
+        BackH,BackW,_=frame.shape
+        added_img=overlay.overlay_transparent(frame,personFrame,int(((BackW-1)/2)-((w-1)/2)),int(((BackH-1)/2)-((h-1)/2)))
+        img = detector.findPose(frame)
+        lmList = detector.findPosition(frame)
+
+        cv2.imshow("frame",added_img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q') :
+        break
+
+webcam.release()
+cv2.destroyAllWindows()
