faf4731ebb9492ef6a6d33ed58023b716f5b94f8
songsuyoung <tndud3999@naver.com>
2023-03-31 12:29:09 +0900

    각도 추출 알고리즘 구현

--- main.py
+++ main.py
@@ -3,39 +3,29 @@ import PoseDetector as pd
 import ImageOverlayer as io
 import math
 import time
+import numpy as np
 
-def FindDistance(jointPos1, jointPos2) :
+def GetJointAngle(CenterPos,jointPos1, jointPos2) :
 
-    _,x1,y1=jointPos1
-    _,x2,y2=jointPos2
-    x=x2-x1
-    y=y2-y1
-    distance=math.sqrt((x*x)+(y*y))
+    theta1=math.atan2((jointPos1[2]-CenterPos[2]), (jointPos1[1]-CenterPos[1]))
+    theta2=math.atan2((jointPos2[2]-CenterPos[2]), (jointPos2[1]-CenterPos[1]))
 
-    return distance
+    degree= abs(theta2-theta1)*180/math.pi
 
-def TransOneDegree(b,c,a):
+    return degree
 
-    b2=math.pow(b,2)
-    c2=math.pow(c,2)
-    a2=math.pow(a,2)
+def MatchTwoAngle(lmList): #두 영상으로 비교 예정...
+    leftUpperBodyAngle={"LelbowAngle":[14,16,12],"LarmpitAngle":[12,14,24],"LbodyAngle":[12,11,24]}
+    rightUpperBodyAngle={"RelbowAngle":[13,11,15],"RarmpitAngle":[11,13,23],"RbodyAngle":[11,12,23]}
 
-    resultDeg=math.degrees(math.acos((b2+c2-a2)/(2*b*c)))
-
-    if resultDeg>180 :
-        return -1  #error넘버 지정하기, 사람의 몸은 180도 이상 측정 불가
-    return resultDeg
-
-def TransDegree(b,c,a):
-
-    return 0
+    return False
 
 cap = cv2.VideoCapture("Shoulder_1.mp4")
 pTime = 0
 detector = pd.PoseDetector()
 
 frameCount=cap.get(cv2.CAP_PROP_FRAME_COUNT)
-print(frameCount)
+
 while True:
     cTime = time.time()
     success, img = cap.read()
@@ -46,13 +36,7 @@ while True:
             img = detector.findPose(img)
             lmList = detector.findPosition(img)
 
-
-            distance1=int(FindDistance(lmList[11],lmList[13])) #b
-            distance2=int(FindDistance(lmList[13], lmList[15]))  #c
-            distance3=int(FindDistance(lmList[11], lmList[15])) #a
-
-            #print(str(distance1)+" "+str(distance2)+" "+str(distance3))
-            print(TransOneDegree(distance1,distance2,distance3))
+            print(MatchTwoAngle(lmList))
 
     cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
     cv2.imshow("image", img)
