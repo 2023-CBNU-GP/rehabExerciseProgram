bbd0e61fe0335de889c690fb63616fff6b83d8d6
songsuyoung <tndud3999@naver.com>
2023-09-12 09:56:01 +0900

    의사 좌표, 환자 좌표간의 길이 일치 알고리즘

--- main.py
+++ main.py
@@ -1,80 +1,113 @@
 import cv2
+import numpy as np
+
+import cv2
 import PoseDetector as pd
-import AngleManager as af
 
+#벡터 1로 정규화
+## 12번을 기준으로 벡터 1로 만들어 정규화 해볼 예정
 
-file_name="test1.mp4"
-cap = cv2.VideoCapture(file_name)
-pTime = 0
-detector = pd.PoseDetector()
+## 1. 기준벡터(의사의 좌표값)을 벡터 1로 정규화한다.
+## 2. 의사의 크기/크기 1을 나눈 n배수를 구한다.
+## 3. 환자의 x,y값을 벡터 1값을 가지는 x,y좌표로 바꾼다. (x/dist,y/dist)
+## 4. 의사의 n배수를 곱하면, 두 크기가 같아진다?
+## ** 의사를 환자한테 맞춘다. 환자 영상을 볼 수 있기 때문 ?
 
-frameCount=cap.get(cv2.CAP_PROP_FRAME_COUNT)
-patientAngle= {"LelbowAngle":0,"LshoulderAngle":0,"RelbowAngle":0,"RshoulderAngle":0
-            ,"Lhip":0,"Rhip":0,"Lknee":0,"Rknee":0}
-scoreAngle= {"LelbowAngle":0,"LshoulderAngle":0,"RelbowAngle":0,"RshoulderAngle":0
-            ,"Lhip":0,"Rhip":0,"Lknee":0,"Rknee":0}
-AngleManager=af.AngleManager()
+#######
+# 12 - 14 | 14 - 16
+# 11 - 13 | 13 - 15
+# 12 - 24 | 11 - 23
+# 24 - 26 | 26 - 28
+# 23 - 25 | 25 - 27
+#######
+def adjustStd(patient, doctor):
+    thresh=[[12,14],[14,16],[11,13],[13,15],[12,24],[11,23],[24,26],[26,28],[23,25],[25,27],[28,32],[28,30],[27,29],[27,31]]
+    #thresh의 리스트 idx번에 해당하는 x,y좌표 리스트를 numpy화
+    #patient Standard 의 줄인말로, 환자 기준치를 의미한다.
 
-teacherAngle=AngleManager.GetAvgAngle("test.mp4")
+    for idx in range(len(thresh)) :
+        patiStd1 = np.array(patient[thresh[idx][0]])
+        patiStd2 = np.array(patient[thresh[idx][1]])
+        patiDist=np.linalg.norm(patiStd1-patiStd2) # 두 좌표간의 길이을 측정하는 함수
 
+        doctStd1 = np.array(doctor[thresh[idx][0]])
+        doctStd2 = np.array(doctor[thresh[idx][1]])
+        doctDist=np.linalg.norm(doctStd1-doctStd2)
 
-poselist={11:[0,0],12:[0,0],13:[0,0],14:[0,0],15:[0,0],16:[0,0],23:[0,0],24:[0,0],25:[0,0],26:[0,0],27:[0,0],28:[0,0]}
+        newDoct2=doctStd1+((doctStd2-doctStd1)/doctDist)*patiDist
+        doctor[thresh[idx][1]]=newDoct2 #새로운 값으로 초기화
 
-frame=0;
-similarity=0.0
-while True:
-    success, img = cap.read()
+    return doctor
 
-    Curframe=cap.get(cv2.CAP_PROP_POS_FRAMES)
+#환자의 동영상에 매치된 의사의 스켈레톤을 보여줌!!!
+#계산된 의사의 좌표값은 사라짐. DB에 저장하지 않는다는 점.
+file_name="test1.mp4" #test1을 환자라고 생각한다.
+detector = pd.PoseDetector() #환자용
+detector1 = pd.PoseDetector() #의사용
 
-    if  Curframe>= frameCount/3 and Curframe<=frameCount-frameCount/3: #현재 프레임 수를 확인 후, 지정된 프레임 이상일 시 동영상에서 스켈렙톤 뽑아내기
-          frame+=1
-          img = detector.findPose(img)
-          lmList = detector.findPosition(img)
+cap = cv2.VideoCapture(file_name) #환자 동영상을 읽는다.
 
-          #사이각 구하기 공식
-          AngleManager.GetAngle(lmList,patientAngle)
-          AngleManager.GetAverageAngle(lmList,patientAngle)
-          AngleManager.ComparePose(teacherAngle,patientAngle,scoreAngle)
-          #cos유사도
-          AngleManager.GetAverageJoint(lmList,poselist)
-          similarity=AngleManager.GetSimiarityCos(teacherAngle,poselist)
+file_name1="test.mp4" #의사용
+cap1 = cv2.VideoCapture(file_name1) #의사의 데이터를 읽는다.
 
-                #의사용, 환자의 경우 실시간으로 비교가 일어나야하므로 필요없음.
+while (cap.isOpened()):
+    success, target_image = cap.read() #환자 동영상을 읽어온다.
+    _,skeleton_image=cap1.read() #의사 동영상을 읽어온다.
 
+    #두 이미지의 크기 정규화 : 의사의 이미지 -> 환자의 이미지 크기로 키우거나 줄임.
+    skeleton_image = cv2.resize(skeleton_image, (target_image.shape[1], target_image.shape[0]))
 
-          cv2.imshow('img',img)
+    skeleton_image = detector.findPose(skeleton_image)     #의사의 이미지로부터 좌표값을 찾는다
+    target_image=detector1.findPose(target_image)       #환자의 이미지로부터 좌표값을 찾는다
 
-    if img is None:
+    lmList,doctor = detector.findPosition(skeleton_image) #의사
+    lmList2,patient = detector1.findPosition(target_image) #환자
+
+    adjustStd(patient, doctor)
+    target_image=detector1.drawPose(target_image,patient,255) #환자 색
+    target_image=detector1.drawPose(target_image,doctor,100)    #의사 색
+
+    if skeleton_image is None:
         break
 
+    cv2.imshow('img2',target_image)
+    # 'q' 키를 누르면 종료
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-print(similarity)
-print(scoreAngle)
+cap.release()
+cv2.destroyAllWindows()
+#송수영 이미지 프레임 2
+#영상 두개 가져와서 길이 일치시키기
+# try:
+#     json_data={}
+#     with open(file_path) as json_file:
+#         json_data=json.load(json_file)
+#
+#         dic={}
+#         dic["jiwoo"]={
+#             "apple":3,"orange":5
+#         }
+#
+#         json_data.update(dic)
+#         print(json_data)
+#
+#         with open('fruit.json','w') as make_file :
+#             fruit=json.dump(json_data,make_file,indent='\t')
+#
+# except :
+#     dic={}
+#     dic["suyoung"]={
+#         "apple":3,"orange":5
+#     }
+#
+#     with open('fruit.json','w') as make_file :
+#         fruit=json.dump(dic,make_file,indent='\t')
 
-# for i,value in patientAngle.items():
-#      patientAngle[i]=round(value/frameCount,2)
-#print(patientAngle)
-# # for id,value in poselist.items():
-# #     x=value[0]
-# #     y=value[1]
-# #
-# #     poselist[id]=[round(x/(frameCount/3),2),round(y/(frameCount/3),2)]
-# #
-# # print(poselist)
-## round해서 나누지말고 그대로 저장해야함..
-# # AngleManager.TransferJsonFile(file_name,poselist,patientAngle)
-# cv2.waitKey(1)
-#
-#
-#
-#
-#
-#
-#
-# #위 다 일치하나, 선생님용은 아래 함수를 적어야함.
-# #AngleManager.TransferJsonFile(file_name,TeacherAngleAvg)
-#
-# cap.release()
-# cv2.destroyAllWindows()
-#
+
+##수학식
+    #c=np.array([1,1])
+    #d=np.array([5,5])
+
+    #dist2=np.linalg.norm(d-c)
+    #c1=c+((d-c)/dist2)*dist
